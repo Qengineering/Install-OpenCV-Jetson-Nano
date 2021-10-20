399d4d50d31aaada2deb463c7b4ee05efcb771a2
Q-engineering <info@qengineering.eu>
2021-10-20 12:41:50 +0200

    Update OpenCV-4-5-3.sh

--- OpenCV-4-5-3.sh
+++ OpenCV-4-5-3.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 echo "Installing OpenCV 4.5.3 on your Jetson Nano"
 echo "It will take 2.5 hours !"
 
@@ -8,20 +10,21 @@ sudo sh -c "echo '/usr/local/cuda/lib64' >> /etc/ld.so.conf.d/nvidia-tegra.conf"
 sudo ldconfig
 
 # install the dependencies
-sudo apt-get install -y build-essential cmake git unzip pkg-config
-sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev
-sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y libgtk2.0-dev libcanberra-gtk*
+sudo apt-get install -y build-essential cmake git unzip pkg-config zlib1g-dev
+sudo apt-get install -y libjpeg-dev libjpeg8-dev libjpeg-turbo8-dev libpng-dev libtiff-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libglew-dev
+sudo apt-get install -y libgtk2.0-dev libgtk-3-dev libcanberra-gtk*
+sudo apt-get install -y python-dev python-numpy python-pip
 sudo apt-get install -y python3-dev python3-numpy python3-pip
 sudo apt-get install -y libxvidcore-dev libx264-dev libgtk-3-dev
-sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev
-sudo apt-get install -y gstreamer1.0-tools libv4l-dev v4l-utils
-sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt-get install -y libavresample-dev libvorbis-dev libxine2-dev
-sudo apt-get install -y libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev libxine2-dev
+sudo apt-get install -y gstreamer1.0-tools libv4l-dev v4l-utils v4l2ucp  qv4l2 
+sudo apt-get install -y libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev
+sudo apt-get install -y libavresample-dev libvorbis-dev libxine2-dev libtesseract-dev
+sudo apt-get install -y libfaac-dev libmp3lame-dev libtheora-dev libpostproc-dev
 sudo apt-get install -y libopencore-amrnb-dev libopencore-amrwb-dev
 sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
-sudo apt-get install -y liblapack-dev libeigen3-dev gfortran
+sudo apt-get install -y liblapack-dev liblapacke-dev libeigen3-dev gfortran
 sudo apt-get install -y libhdf5-dev protobuf-compiler
 sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
 
@@ -63,6 +66,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D ENABLE_NEON=ON \
 -D WITH_QT=OFF \
 -D WITH_OPENMP=ON \
+-D WITH_OPENGL=ON \
 -D BUILD_TIFF=ON \
 -D WITH_FFMPEG=ON \
 -D WITH_GSTREAMER=ON \
@@ -85,6 +89,7 @@ FREE_MEM="$(free -m | awk '/^Swap/ {print $2}')"
 if [[ "FREE_MEM" -gt "5500" ]]; then
   NO_JOB=4
 else
+  echo "Due to limited swap, make only uses 1 core"
   NO_JOB=1
 fi
 make -j ${NO_JOB} 
