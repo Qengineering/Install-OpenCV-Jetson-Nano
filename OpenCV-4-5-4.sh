b7ce0603376ca1f72261eb09cae67119cf51e5d6
Q-engineering <info@qengineering.eu>
2023-07-12 15:34:05 +0200

    Update OpenCV-4-5-4.sh

--- OpenCV-4-5-4.sh
+++ OpenCV-4-5-4.sh
@@ -14,7 +14,6 @@ sudo apt-get install -y build-essential cmake git unzip pkg-config zlib1g-dev
 sudo apt-get install -y libjpeg-dev libjpeg8-dev libjpeg-turbo8-dev libpng-dev libtiff-dev
 sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libglew-dev
 sudo apt-get install -y libgtk2.0-dev libgtk-3-dev libcanberra-gtk*
-sudo apt-get install -y python-dev python-numpy python-pip
 sudo apt-get install -y python3-dev python3-numpy python3-pip
 sudo apt-get install -y libxvidcore-dev libx264-dev libgtk-3-dev
 sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev libxine2-dev
