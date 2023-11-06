91b355ea70b1274b5d82c67c9e61b3237be53681
Q-engineering <info@qengineering.eu>
2023-11-06 09:35:40 +0100

    Update OpenCV-4-8-0.sh

--- OpenCV-4-8-0.sh
+++ OpenCV-4-8-0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+install_opencv () {
 echo "Installing OpenCV 4.8.0 on your Jetson Nano"
 echo "It will take 3.5 hours !"
 
@@ -94,3 +94,25 @@ sudo apt-get update
 
 echo "Congratulations!"
 echo "You've successfully installed OpenCV 4.8.0 on your Jetson Nano"
+}
+
+cd ~
+
+if [ -d ~/opencv/build ]; then
+  echo " "
+  echo "You have a directory ~/opencv/build on your disk."
+  echo "Continuing the installation will replace this folder."
+  echo " "
+  
+  printf "Do you wish to continue (Y/n)?"
+  read answer
+
+  if [ "$answer" != "${answer#[Nn]}" ] ;then 
+      echo "Leaving without installing OpenCV"
+  else
+      install_opencv
+  fi
+else
+    install_opencv
+fi
+
