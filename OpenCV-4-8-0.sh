70d0a3c24dfc9837076b8c09384a8713c3f4f6e8
Q-engineering <info@qengineering.eu>
2023-11-15 13:24:20 +0100

    Update OpenCV-4-8-0.sh

--- OpenCV-4-8-0.sh
+++ OpenCV-4-8-0.sh
@@ -9,8 +9,22 @@ install_opencv () {
       echo ""
       if [[ $model == *"Orin Nano"* ]]; then
           echo "Detecting a Jetson Nano Orin."
+	  # Use always "-j 4"
+          NO_JOB=4
+          ARCH=8.7
+          PTX="sm_87"
       elif [[ $model == *"Jetson Nano"* ]]; then
           echo "Detecting a regular Jetson Nano."
+          ARCH=5.3
+          PTX="sm_53"
+	  # Use "-j 4" only swap space is larger than 5.5GB
+	  FREE_MEM="$(free -m | awk '/^Swap/ {print $2}')"
+	  if [[ "FREE_MEM" -gt "5500" ]]; then
+	    NO_JOB=4
+	  else
+	    echo "Due to limited swap, make only uses 1 core"
+	    NO_JOB=1
+	  fi
       else
           echo "Unable to determine the Jetson Nano model."
           exit 1
@@ -67,86 +81,42 @@ install_opencv () {
   # set install dir
   cd ~/opencv
   mkdir build
-  cd build
+  cd builds
   
   # run cmake
-  if [[ $model == *"Orin Nano"* ]]; then
-    cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-    -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 \
-    -D WITH_OPENCL=OFF \
-    -D CUDA_ARCH_BIN=8.7 \
-    -D CUDA_ARCH_PTX="sm_87" \
-    -D WITH_CUDA=ON \
-    -D WITH_CUDNN=ON \
-    -D WITH_CUBLAS=ON \
-    -D ENABLE_FAST_MATH=ON \
-    -D CUDA_FAST_MATH=ON \
-    -D OPENCV_DNN_CUDA=ON \
-    -D ENABLE_NEON=ON \
-    -D WITH_QT=OFF \
-    -D WITH_OPENMP=ON \
-    -D BUILD_TIFF=ON \
-    -D WITH_FFMPEG=ON \
-    -D WITH_GSTREAMER=ON \
-    -D WITH_TBB=ON \
-    -D BUILD_TBB=ON \
-    -D BUILD_TESTS=OFF \
-    -D WITH_EIGEN=ON \
-    -D WITH_V4L=ON \
-    -D WITH_LIBV4L=ON \
-    -D WITH_PROTOBUF=ON \
-    -D OPENCV_ENABLE_NONFREE=ON \
-    -D INSTALL_C_EXAMPLES=OFF \
-    -D INSTALL_PYTHON_EXAMPLES=OFF \
-    -D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages \
-    -D OPENCV_GENERATE_PKGCONFIG=ON \
-    -D BUILD_EXAMPLES=OFF ..  
-  else
-    cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-    -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 \
-    -D WITH_OPENCL=OFF \
-    -D CUDA_ARCH_BIN=5.3 \
-    -D CUDA_ARCH_PTX="sm_53" \
-    -D WITH_CUDA=ON \
-    -D WITH_CUDNN=ON \
-    -D WITH_CUBLAS=ON \
-    -D ENABLE_FAST_MATH=ON \
-    -D CUDA_FAST_MATH=ON \
-    -D OPENCV_DNN_CUDA=ON \
-    -D ENABLE_NEON=ON \
-    -D WITH_QT=OFF \
-    -D WITH_OPENMP=ON \
-    -D BUILD_TIFF=ON \
-    -D WITH_FFMPEG=ON \
-    -D WITH_GSTREAMER=ON \
-    -D WITH_TBB=ON \
-    -D BUILD_TBB=ON \
-    -D BUILD_TESTS=OFF \
-    -D WITH_EIGEN=ON \
-    -D WITH_V4L=ON \
-    -D WITH_LIBV4L=ON \
-    -D WITH_PROTOBUF=ON \
-    -D OPENCV_ENABLE_NONFREE=ON \
-    -D INSTALL_C_EXAMPLES=OFF \
-    -D INSTALL_PYTHON_EXAMPLES=OFF \
-    -D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages \
-    -D OPENCV_GENERATE_PKGCONFIG=ON \
-    -D BUILD_EXAMPLES=OFF ..
-  fi
-  
-  # run make
-  FREE_MEM="$(free -m | awk '/^Swap/ {print $2}')"
-  # Use "-j 4" only swap space is larger than 5.5GB
-  if [[ "FREE_MEM" -gt "5500" ]]; then
-    NO_JOB=4
-  else
-    echo "Due to limited swap, make only uses 1 core"
-    NO_JOB=1
-  fi
+  cmake -D CMAKE_BUILD_TYPE=RELEASE \
+  -D CMAKE_INSTALL_PREFIX=/usr \
+  -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+  -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 \
+  -D WITH_OPENCL=OFF \
+  -D CUDA_ARCH_BIN=${ARCH} \
+  -D CUDA_ARCH_PTX=${PTX} \
+  -D WITH_CUDA=ON \
+  -D WITH_CUDNN=ON \
+  -D WITH_CUBLAS=ON \
+  -D ENABLE_FAST_MATH=ON \
+  -D CUDA_FAST_MATH=ON \
+  -D OPENCV_DNN_CUDA=ON \
+  -D ENABLE_NEON=ON \
+  -D WITH_QT=OFF \
+  -D WITH_OPENMP=ON \
+  -D BUILD_TIFF=ON \
+  -D WITH_FFMPEG=ON \
+  -D WITH_GSTREAMER=ON \
+  -D WITH_TBB=ON \
+  -D BUILD_TBB=ON \
+  -D BUILD_TESTS=OFF \
+  -D WITH_EIGEN=ON \
+  -D WITH_V4L=ON \
+  -D WITH_LIBV4L=ON \
+  -D WITH_PROTOBUF=ON \
+  -D OPENCV_ENABLE_NONFREE=ON \
+  -D INSTALL_C_EXAMPLES=OFF \
+  -D INSTALL_PYTHON_EXAMPLES=OFF \
+  -D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages \
+  -D OPENCV_GENERATE_PKGCONFIG=ON \
+  -D BUILD_EXAMPLES=OFF ..
+ 
   make -j ${NO_JOB} 
   
   sudo rm -r /usr/include/opencv4/opencv2
@@ -180,4 +150,3 @@ if [ -d ~/opencv/build ]; then
 else
     install_opencv
 fi
-
