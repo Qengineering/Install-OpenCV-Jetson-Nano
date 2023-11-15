4001082d21e714a55908b9e1856528ccba573dc9
Q-engineering <info@qengineering.eu>
2023-11-15 10:38:24 +0100

    Update OpenCV-4-8-0.sh

--- OpenCV-4-8-0.sh
+++ OpenCV-4-8-0.sh
@@ -1,99 +1,132 @@
 #!/bin/bash
 set -e
 install_opencv () {
-echo "Installing OpenCV 4.8.0 on your Jetson Nano"
-echo "It will take 3.5 hours !"
-
-# reveal the CUDA location
-cd ~
-sudo sh -c "echo '/usr/local/cuda/lib64' >> /etc/ld.so.conf.d/nvidia-tegra.conf"
-sudo ldconfig
-
-# install the dependencies
-sudo apt-get install -y build-essential cmake git unzip pkg-config zlib1g-dev
-sudo apt-get install -y libjpeg-dev libjpeg8-dev libjpeg-turbo8-dev libpng-dev libtiff-dev
-sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libglew-dev
-sudo apt-get install -y libgtk2.0-dev libgtk-3-dev libcanberra-gtk*
-sudo apt-get install -y python3-dev python3-numpy python3-pip
-sudo apt-get install -y libxvidcore-dev libx264-dev libgtk-3-dev
-sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev libxine2-dev
-sudo apt-get install -y gstreamer1.0-tools libv4l-dev v4l-utils qv4l2 
-sudo apt-get install -y libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev
-sudo apt-get install -y libavresample-dev libvorbis-dev libxine2-dev libtesseract-dev
-sudo apt-get install -y libfaac-dev libmp3lame-dev libtheora-dev libpostproc-dev
-sudo apt-get install -y libopencore-amrnb-dev libopencore-amrwb-dev
-sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
-sudo apt-get install -y liblapack-dev liblapacke-dev libeigen3-dev gfortran
-sudo apt-get install -y libhdf5-dev protobuf-compiler
-sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
-
-# remove old versions or previous builds
-cd ~ 
-sudo rm -rf opencv*
-# download the latest version
-git clone --depth=1 https://github.com/opencv/opencv.git
-git clone --depth=1 https://github.com/opencv/opencv_contrib.git
-
-# set install dir
-cd ~/opencv
-mkdir build
-cd build
-
-# run cmake
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
--D CMAKE_INSTALL_PREFIX=/usr \
--D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
--D EIGEN_INCLUDE_PATH=/usr/include/eigen3 \
--D WITH_OPENCL=OFF \
--D WITH_CUDA=ON \
--D CUDA_ARCH_BIN=5.3 \
--D CUDA_ARCH_PTX="" \
--D WITH_CUDNN=ON \
--D WITH_CUBLAS=ON \
--D ENABLE_FAST_MATH=ON \
--D CUDA_FAST_MATH=ON \
--D OPENCV_DNN_CUDA=ON \
--D ENABLE_NEON=ON \
--D WITH_QT=OFF \
--D WITH_OPENMP=ON \
--D BUILD_TIFF=ON \
--D WITH_FFMPEG=ON \
--D WITH_GSTREAMER=ON \
--D WITH_TBB=ON \
--D BUILD_TBB=ON \
--D BUILD_TESTS=OFF \
--D WITH_EIGEN=ON \
--D WITH_V4L=ON \
--D WITH_LIBV4L=ON \
--D WITH_PROTOBUF=ON \
--D OPENCV_ENABLE_NONFREE=ON \
--D INSTALL_C_EXAMPLES=OFF \
--D INSTALL_PYTHON_EXAMPLES=OFF \
--D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages \
--D OPENCV_GENERATE_PKGCONFIG=ON \
--D BUILD_EXAMPLES=OFF ..
-
-# run make
-FREE_MEM="$(free -m | awk '/^Swap/ {print $2}')"
-# Use "-j 4" only swap space is larger than 5.5GB
-if [[ "FREE_MEM" -gt "5500" ]]; then
-  NO_JOB=4
-else
-  echo "Due to limited swap, make only uses 1 core"
-  NO_JOB=1
-fi
-make -j ${NO_JOB} 
-
-sudo rm -r /usr/include/opencv4/opencv2
-sudo make install
-sudo ldconfig
-
-# cleaning (frees 320 MB)
-make clean
-sudo apt-get update
-
-echo "Congratulations!"
-echo "You've successfully installed OpenCV 4.8.0 on your Jetson Nano"
+  # Check if the file /proc/device-tree/model exists
+  if [ -e "/proc/device-tree/model" ]; then
+      # Read the model information from /proc/device-tree/model and remove null bytes
+      model=$(tr -d '\0' < /proc/device-tree/model)
+      # Check if the model information contains "Jetson Nano Orion"
+      if [[ $model == *"Orin Nano"* ]]; then
+          echo "Detecting a Jetson Nano Orin."
+      elif [[ $model == *"Jetson Nano"* ]]; then
+          echo "Detecting a regular Jetson Nano."
+      else
+          echo "Unable to determine the Jetson Nano model."
+          exit 1
+      fi
+  else
+      echo "Error: /proc/device-tree/model not found. Are you sure this is a Jetson Nano?"
+      exit 1
+  fi
+  
+  echo "Installing OpenCV 4.8.0 on your Nano"
+  echo "It will take 3.5 hours !"
+  
+  # reveal the CUDA location
+  cd ~
+  sudo sh -c "echo '/usr/local/cuda/lib64' >> /etc/ld.so.conf.d/nvidia-tegra.conf"
+  sudo ldconfig
+  
+  # install the Jetson Nano dependencies first
+  if [[ $model == *"Jetson Nano"* ]]; then
+    sudo apt-get install -y build-essential git unzip pkg-config zlib1g-dev
+    sudo apt-get install -y python3-dev python3-numpy
+    sudo apt-get install -y python-dev python-numpy python-pip
+    sudo apt-get install -y gstreamer1.0-tools libgstreamer-plugins-base1.0-dev
+    sudo apt-get install -y libgstreamer-plugins-good1.0-dev
+    sudo apt-get install -y libtbb2 libgtk-3-dev v4l2ucp libxine2-dev
+  fi
+  # install the common dependencies
+  sudo apt-get install -y cmake
+  sudo apt-get install -y libjpeg-dev libjpeg8-dev libjpeg-turbo8-dev
+  sudo apt-get install -y libpng-dev libtiff-dev libglew-dev
+  sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
+  sudo apt-get install -y libgtk2.0-dev libgtk-3-dev libcanberra-gtk*
+  sudo apt-get install -y python3-pip
+  sudo apt-get install -y libxvidcore-dev libx264-dev
+  sudo apt-get install -y libtbb-dev libdc1394-22-dev libxine2-dev
+  sudo apt-get install -y libv4l-dev v4l-utils qv4l2
+  sudo apt-get install -y libtesseract-dev libpostproc-dev
+  sudo apt-get install -y libavresample-dev libvorbis-dev
+  sudo apt-get install -y libfaac-dev libmp3lame-dev libtheora-dev
+  sudo apt-get install -y libopencore-amrnb-dev libopencore-amrwb-dev
+  sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
+  sudo apt-get install -y liblapack-dev liblapacke-dev libeigen3-dev gfortran
+  sudo apt-get install -y libhdf5-dev libprotobuf-dev protobuf-compiler
+  sudo apt-get install -y libgoogle-glog-dev libgflags-dev
+ 
+  # remove old versions or previous builds
+  cd ~ 
+  sudo rm -rf opencv*
+  # download the latest version
+  git clone --depth=1 https://github.com/opencv/opencv.git
+  git clone --depth=1 https://github.com/opencv/opencv_contrib.git
+  
+  # set install dir
+  cd ~/opencv
+  mkdir build
+  cd build
+  
+  # run cmake
+  cmake -D CMAKE_BUILD_TYPE=RELEASE \
+  -D CMAKE_INSTALL_PREFIX=/usr \
+  -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+  -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 \
+  -D WITH_OPENCL=OFF \
+  if [[ $model == *"Orin Nano"* ]]; then
+    -D CUDA_ARCH_BIN=8.7 \
+    -D CUDA_ARCH_PTX="sm_87" \
+  else
+    -D CUDA_ARCH_BIN=5.3 \
+    -D CUDA_ARCH_PTX="sm_53" \
+  fi
+  -D WITH_CUDA=ON \
+  -D WITH_CUDNN=ON \
+  -D WITH_CUBLAS=ON \
+  -D ENABLE_FAST_MATH=ON \
+  -D CUDA_FAST_MATH=ON \
+  -D OPENCV_DNN_CUDA=ON \
+  -D ENABLE_NEON=ON \
+  -D WITH_QT=OFF \
+  -D WITH_OPENMP=ON \
+  -D BUILD_TIFF=ON \
+  -D WITH_FFMPEG=ON \
+  -D WITH_GSTREAMER=ON \
+  -D WITH_TBB=ON \
+  -D BUILD_TBB=ON \
+  -D BUILD_TESTS=OFF \
+  -D WITH_EIGEN=ON \
+  -D WITH_V4L=ON \
+  -D WITH_LIBV4L=ON \
+  -D WITH_PROTOBUF=ON \
+  -D OPENCV_ENABLE_NONFREE=ON \
+  -D INSTALL_C_EXAMPLES=OFF \
+  -D INSTALL_PYTHON_EXAMPLES=OFF \
+  -D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages \
+  -D OPENCV_GENERATE_PKGCONFIG=ON \
+  -D BUILD_EXAMPLES=OFF ..
+  
+  # run make
+  FREE_MEM="$(free -m | awk '/^Swap/ {print $2}')"
+  # Use "-j 4" only swap space is larger than 5.5GB
+  if [[ "FREE_MEM" -gt "5500" ]]; then
+    NO_JOB=4
+  else
+    echo "Due to limited swap, make only uses 1 core"
+    NO_JOB=1
+  fi
+  make -j ${NO_JOB} 
+  
+  sudo rm -r /usr/include/opencv4/opencv2
+  sudo make install
+  sudo ldconfig
+  
+  # cleaning (frees 320 MB)
+  make clean
+  sudo apt-get update
+  
+  echo "Congratulations!"
+  echo "You've successfully installed OpenCV 4.8.0 on your Nano"
 }
 
 cd ~
